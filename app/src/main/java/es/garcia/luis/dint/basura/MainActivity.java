18a17cf0f1d52dba4e108a8c7c53c1a987b348ed
Luis García <luisgasa_uax@gmail.com>
2023-10-19 10:29:55 +0200

    CAGÁNDONOS EN TODO

---
+++ app/src/main/java/es/garcia/luis/dint/basura/MainActivity.java
@@ -0,0 +1,19 @@
+package es.garcia.luis.dint.basura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        /*
+        ME CAGO EN TU PUTA MADRE!!!
+         */
+    }
+}
